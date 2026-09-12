81ab3df9b585ce9262ac62552ff728384a9d4528
Aliaksandr <grubian2@gmail.com>
2026-09-12 12:36:04 +0300

    cmake-bootstrap: use flags instead of overriding phase

--- pkgs/cm/cmake-bootstrap/package.nix
+++ pkgs/cm/cmake-bootstrap/package.nix
@@ -1,22 +1,20 @@
 # cmake built by its ./bootstrap (make, bundled libraries), for the build machine only: the cmake
 # that builds zlib, zstd, expat, curl, libarchive and then cmake proper, which link those
-{ package }:
+{ package, toolchain }:
 package {
   name = "cmake-bootstrap";
   uses = [ "autotools" ];
   autotools.configureScript = "bootstrap";
-  phases = [
-    {
-      name = "configure";
-      run = ''
-        cd $c.build
-        (x $env.CONFIG_SHELL $"($c.src)/bootstrap" $"--prefix=($c.out)" $"--parallel=($c.njobs)" --no-system-libs
-          --no-qt-gui --docdir=share/doc/cmake --mandir=share/man
-          -- -DCMAKE_USE_OPENSSL=OFF -DBUILD_TESTING=OFF -DCMake_BUILD_LTO=OFF $"-DCMAKE_SYSTEM_PREFIX_PATH=($c.platform.sysroot)")
-      '';
-    }
-    "autotools.build"
-    "autotools.install"
+  autotools.flags = [
+    "--no-system-libs"
+    "--no-qt-gui"
+    "--docdir=share/doc/cmake"
+    "--mandir=share/man"
+    "--"
+    "-DCMAKE_USE_OPENSSL=OFF"
+    "-DBUILD_TESTING=OFF"
+    "-DCMake_BUILD_LTO=OFF"
+    "-DCMAKE_SYSTEM_PREFIX_PATH=${toolchain.sysroot}"
   ];
   platforms.cross = false;
   tests.run = false;
